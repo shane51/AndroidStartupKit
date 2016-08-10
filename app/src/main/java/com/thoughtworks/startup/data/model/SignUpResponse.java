3c3b50478160901eadf173c7a893b5547bf6c197
shane51 <suse51@163.com>
2016-08-10 11:44:05 +0800

    Add response model for Signup

---
+++ app/src/main/java/com/thoughtworks/startup/data/model/SignUpResponse.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.startup.data.model;
+
+public class SignUpResponse {
+
+    private Integer status;
+    private String msg;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+}
